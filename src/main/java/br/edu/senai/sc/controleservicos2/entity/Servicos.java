b50e7a54efdcac845beb7128f453be7d6fa99f15
Ruhan <ruhan_350@hotmail.com>
2023-05-01 17:37:40 -0300

    Implementado a consulta de todos os servicos

--- src/main/java/br/edu/senai/sc/controleservicos2/entity/Servicos.java
+++ src/main/java/br/edu/senai/sc/controleservicos2/entity/Servicos.java
@@ -15,7 +15,6 @@ import java.util.List;
 @Entity
 public class Servicos {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigoServico;
     private String descricao;
     private Double valorClaro;
